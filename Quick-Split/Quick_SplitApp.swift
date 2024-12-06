d071b8d13ba762401fa837d27251cfa0dc5e7fff
Denidu Gamage <denidugamage21@gmail.com>
2024-12-06 18:59:24 +0530

    Initial Commit

---
+++ Quick-Split/Quick_SplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quick_SplitApp.swift
+//  Quick-Split
+//
+//  Created by Denidu Gamage on 2024-12-06.
+//
+
+import SwiftUI
+
+@main
+struct Quick_SplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Quick-SplitTests/Quick_SplitTests.swift
@@ -0,0 +1,16 @@
+//
+//  Quick_SplitTests.swift
+//  Quick-SplitTests
+//
+//  Created by Denidu Gamage on 2024-12-06.
+//
+
+import Testing
+
+struct Quick_SplitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
